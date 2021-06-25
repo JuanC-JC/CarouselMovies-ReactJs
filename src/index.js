d7b9fdaba6ad603c6ad2eaa54400bfc1dfb56dfa
JuanC-JC <juancchaparro97@gmail.com>
2021-06-25 00:08:45 -0500

    configuracion inical del proyecto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./components/styles/s.scss";
+
+// console.log('asdasd')
+function App() {
+  return <h1>test</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:'./src/index.js',
+
+    output:{
+        path:path.resolve(__dirname,'build'),
+        filename:'bundle.js'
+    },
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.s?css$/i,
+                use: [{
+                    loader:MiniCssExtractPlugin.loader
+                },
+                "css-loader",
+                "sass-loader"
+                ]
+            },
+            {
+                test:/\.html$/i,
+                use:{
+                    loader:'html-loader'
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'assets/[name].css'
+        })
+    ]
+
+}
